858adf6593d366c9285f1039f1c11f6274e10548
love.cosma <williamsmatthew333@gmail.com>
2021-12-09 16:39:14 -0700

    Optimizing for production.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  scope :api do 
+    resources :birds
+  end 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
